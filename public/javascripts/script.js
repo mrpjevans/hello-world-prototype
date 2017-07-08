96d88c290a6d40bce2ff2b9e49d25c2e55681b11
PJ Evans <pj@mrpjevans.com>
2017-07-08 15:55:54 +0100

    Client-side JS converted to ES6

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,12 +6,12 @@ function startSocket() {
     socket = io.connect(); 
   
     // On receiving a status change message, update the relevant user's status
-    socket.on('statusChange', function(data) {
+    socket.on('statusChange', (data) => {
         $('span[data-userid=' + data.id + ']').html(data.status);
     });
 
     // On receiving a time broadcast, update the clock and date
-    socket.on('time', function(data) {
+    socket.on('time', (data) => {
         $('.mypanel-clock').html(data.time);
         $('.date-date').html(data.date);
         $('.date-month').html(data.month);
@@ -21,9 +21,9 @@ function startSocket() {
 
 // Attach a click event to the user's status panel
 function initSetStatus(id) {
-    $('li.set-status a').on('click', function(e) {
+    $('li.set-status a').on('click', (e) => {
         // Get the newly selected status and update my panel
-        var newStatus = $(e.target).html();
+        const newStatus = $(e.target).html();
         $('#myStatus').html(newStatus);
         // Emit the new status to the server
         var payload = {'id': id, 'status': newStatus};
@@ -33,7 +33,7 @@ function initSetStatus(id) {
 
 // Call our weather resource and update the weather panel
 function getWeather() {
-    $.getJSON('/weather', function(data) {
-        $('.mypanel-weather').html('<img src="' + data.current_observation.icon_url + '"><p>' + data.current_observation.weather + '</p><p>' + data.current_observation.temperature_string + '</p>');
+    $.getJSON('/weather', (data) => {
+        $('.mypanel-weather').html(`<img src="${data.current_observation.icon_url}"><p>${data.current_observation.weather}</p><p>${data.current_observation.temperature_string}</p>`);
     });
 }
